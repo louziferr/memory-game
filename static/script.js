a68da1bb7c5daa684add7c5d752ed9d684f406fe
luzieahrens <luzie.ahrens@gmail.com>
2025-07-18 09:28:16 +0200

    finish first prototype

--- static/script.js
+++ static/script.js
@@ -8,13 +8,26 @@ for (let i=1; i <=24; i++) {
 // Karten mischen
 karten_array.sort(() => Math.random() - 0.5);
 
+console.log(karten_array);
 
 for (let i of karten_array) {
     // neue Memory Karte erstellen
     const memoryCard = document.createElement("div");
     memoryCard.classList.add("memory-card");
-    memoryCard.innerHTML = `<img src="/static/img/3.svg" alt="card" />`;
-
+    memoryCard.innerHTML = `<img 
+                            src="/static/img/${i%12+1}.svg" 
+                            alt="card" 
+                            class="hidden"
+                            id=${i} />`;
+    // Event-Listener für Klick
+    memoryCard.addEventListener('click', () => handleClick(i));
     // Karte hinzufügen
     gameboard.appendChild(memoryCard);
+}
+
+// Funktion, wenn Karte angeklickt wird
+function handleClick(i) {
+    console.log(i);
+    const karte = document.getElementById(i);
+    karte.classList.toggle('hidden');
 }
